b9cced3fe9c11d8ca702fbb676b30dffc79af86e
Mohamed <mohamed.a.salah11@gmail.com>
2020-07-15 20:48:25 +0200

    changing style of the code

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,13 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-class Processor {
- public:
-  float Utilization();  // TODO: See src/processor.cpp
+class Processor
+{
+  public:
+	float Utilization(); // TODO: See src/processor.cpp
 
-  // TODO: Declare any necessary private members
- private:
+	// TODO: Declare any necessary private members
+  private:
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "ncurses_display.h"
 #include "system.h"
 
-int main() {
-  System system;
-  NCursesDisplay::Display(system);
+int
+main()
+{
+	System system;
+	NCursesDisplay::Display(system);
 }
