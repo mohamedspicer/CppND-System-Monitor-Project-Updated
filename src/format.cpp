388a30ecfa2a379b72e53981ca1e7ea19c2f6c61
Mohamed <mohamed.a.salah11@gmail.com>
2020-07-16 18:14:24 +0200

    formatting uptime

--- src/format.cpp
+++ src/format.cpp
@@ -1,9 +1,12 @@
-#include <string>
 #include <iomanip>
+#include <string>
 
 #include "format.h"
 
+using std::setfill;
+using std::setw;
 using std::string;
+
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
@@ -12,12 +15,14 @@ string
 Format::ElapsedTime(long seconds [[maybe_unused]])
 {
 	long h, m, s;
-	h		 = seconds / 3600;
-	seconds	 = seconds % 3600;
-	m		 = seconds / 60;
-	seconds	 = seconds % 60;
-	s		 = seconds;
-    std::stringstream ss;
-	ss << h << ":" << m << ":" << s ;
+	h		= seconds / 3600;
+	seconds = seconds % 3600;
+	m		= seconds / 60;
+	seconds = seconds % 60;
+	s		= seconds;
+	std::stringstream ss;
+	ss << setw(2) << setfill('0') << h << ":" << setw(2) << setfill('0') << m
+	   << ":" << setw(2) << setfill('0') << s;
+
 	return ss.str();
 }
