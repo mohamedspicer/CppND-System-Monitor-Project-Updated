a9b33e342b8f9b2f29ab5ee16a4ad9ce5a9d7a5a
Mohamed <mohamed.a.salah11@gmail.com>
2020-07-16 18:06:31 +0200

    add uptime in seconds

--- src/format.cpp
+++ src/format.cpp
@@ -4,7 +4,6 @@
 #include "format.h"
 
 using std::string;
-using std::format;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
@@ -18,6 +17,7 @@ Format::ElapsedTime(long seconds [[maybe_unused]])
 	m		 = seconds / 60;
 	seconds	 = seconds % 60;
 	s		 = seconds;
-	string ss = format("{}:{}:{}", h, m, s);
-	return ss;
+    std::stringstream ss;
+	ss << h << ":" << m << ":" << s ;
+	return ss.str();
 }
